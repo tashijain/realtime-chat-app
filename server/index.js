377e9c959d79f85a53071501fd58e441e315ee94
Tashi Jain <tashjain88@gmail.com>
2020-08-01 14:43:53 -0700

    admin messages server side

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
+
 const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
@@ -15,11 +17,27 @@ const io = socketio(server);
 // register client joining and leaving
 // callback function ahead of 'connection'
 io.on("connection", (socket) => {
-  console.log("We have a new connection!!");
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
+    const { user, error } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    // telling user that they are welcome to the chat
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+
+    // will send message to every user besides this user who has joined
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    // joins a user in the room
+    socket.join(user.room);
 
     // error handling or any callbakc after a specific event has been emitted
+    callback();
   });
 
   socket.on("disconnect", () => {
