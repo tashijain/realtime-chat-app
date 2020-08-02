5ced178ed1b0c09c65be767b865f214a02f7579c
Tashi Jain <tashjain88@gmail.com>
2020-08-01 18:26:07 -0700

    deployed with chat changes

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 Http reqests ares low, good for serving up webstes but need webs ockets as used by socket.io to initiate real-time functioning
 
 using query stringsi.e retirve data from the URL and not through props or redux
+
+https://realtimechatbytashi.netlify.app
--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -21,7 +21,7 @@ const Chat = ({ location }) => {
   const [messages, setMessages] = useState([]);
   const [users, setUsers] = useState("");
 
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://realtime-chat-appl-react.herokuapp.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
